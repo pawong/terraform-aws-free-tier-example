4625e4bbc5a56845f726f9e019ff1349d4d71113
Paul Wong <paul@jaiken.com>
2024-05-07 10:37:19 -0700

    working

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -10,22 +10,27 @@ module "vpc" {
 module "subnets" {
   source = "../modules/subnets"
 
-  vpc_id = module.vpc.vpc_id
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
 }
 
 module "security_group" {
   source = "../modules/security-group"
 
-  vpc_id = module.vpc.vpc_id
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
 }
 
 module "iam" {
   source = "../modules/iam"
+
+  project_name = var.project_name
 }
 
 module "lambda" {
   source = "../modules/lambda"
 
+  project_name      = var.project_name
   vpc_id            = module.vpc.vpc_id
   iam_role_arn      = module.iam.role_arn
   subnet_ids        = module.subnets.subnet_ids
